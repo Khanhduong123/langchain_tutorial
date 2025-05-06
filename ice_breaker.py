2f479f407c9754cf86bac4d6e58ceec2f6bc1c5d
Khanhduong123 <khanhbaoduong789@gmail.com>
2025-05-06 09:38:14 +0700

    KhanhDB: update all

--- ice_breaker.py
+++ ice_breaker.py
@@ -4,7 +4,8 @@ from template.prompt import summary_template
 from langchain_openai import ChatOpenAI
 from craw.linkedin import scrape_linkedin_profile_with_proxy
 from agent.linkedin_lookup_agent import lookup as linkedin_lookup_agent
-
+from agent.tweets_lookup_agent import lookup as twitter_lookup_agent
+from craw.twitter import  scrape_user_tweets_mook
 
 
 
@@ -12,12 +13,15 @@ def ice_break_with(name:str)-> str:
     linkedin_username = linkedin_lookup_agent(name)
     linkedin_data = scrape_linkedin_profile_with_proxy(linkedin_username, mock_proxy=True)
     
+
+    twitter_username = twitter_lookup_agent(name=name)
+    tweets = scrape_user_tweets_mook(username=twitter_username)
     llm = ChatOpenAI(
         model="gpt-3.5-turbo", temperature=0, api_key=os.getenv("OPENAI_API_KEY")
     )
     summary_prompt_temaplate = summary_template()
     chain = summary_prompt_temaplate | llm
-    result = chain.invoke(input={"information": linkedin_data})
+    result = chain.invoke(input={"information": linkedin_data, "twitter_post":tweets})
     return result
 
 if __name__ == "__main__":
--- template/prompt.py
+++ template/prompt.py
@@ -2,12 +2,14 @@ from langchain_core.prompts import PromptTemplate
 
 def summary_template() -> str:
     summary_template = """
-        Give the Linkedin informattion {information} about a person from I want you to create:
+        Give the Linkedin informattion {information}, and twitter post {twitter_post} about a person from I want you to create:
         1. a short summary
         2. two interesting facts about them
+
+        Use the both information from the Linkedin and Twitter to create a summary.
     """
 
     return  PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information","twitter_post"], template=summary_template
     )
     
