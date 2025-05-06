dd2a0b9db52437199ebbde02614bad95db184c6d
Khanhduong123 <khanhbaoduong789@gmail.com>
2025-05-06 13:22:03 +0700

    KhanhDB: update all

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,15 +1,16 @@
 import os
+from typing import Tuple
 from dotenv import load_dotenv
-from template.prompt import summary_template
+from prompt_template.prompt import summary_template
 from langchain_openai import ChatOpenAI
 from craw.linkedin import scrape_linkedin_profile_with_proxy
 from agent.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from agent.tweets_lookup_agent import lookup as twitter_lookup_agent
 from craw.twitter import  scrape_user_tweets_mook
+from prompt_template.output_parser import Summary,summary_parser
 
 
-
-def ice_break_with(name:str)-> str:
+def ice_break_with(name:str)-> Tuple[Summary, str]:
     linkedin_username = linkedin_lookup_agent(name)
     linkedin_data = scrape_linkedin_profile_with_proxy(linkedin_username, mock_proxy=True)
     
@@ -20,9 +21,9 @@ def ice_break_with(name:str)-> str:
         model="gpt-3.5-turbo", temperature=0, api_key=os.getenv("OPENAI_API_KEY")
     )
     summary_prompt_temaplate = summary_template()
-    chain = summary_prompt_temaplate | llm
-    result = chain.invoke(input={"information": linkedin_data, "twitter_post":tweets})
-    return result
+    chain = summary_prompt_temaplate | llm | summary_parser
+    result:Summary = chain.invoke(input={"information": linkedin_data, "twitter_post":tweets})
+    return result, linkedin_data.get("profile_pic_url")
 
 if __name__ == "__main__":
     load_dotenv()
--- prompt_template/prompt.py
+++ prompt_template/prompt.py
@@ -1,5 +1,5 @@
 from langchain_core.prompts import PromptTemplate
-
+from .output_parser import summary_parser
 def summary_template() -> str:
     summary_template = """
         Give the Linkedin informattion {information}, and twitter post {twitter_post} about a person from I want you to create:
@@ -7,9 +7,11 @@ def summary_template() -> str:
         2. two interesting facts about them
 
         Use the both information from the Linkedin and Twitter to create a summary.
+        \n{format_instructions}
     """
 
     return  PromptTemplate(
-        input_variables=["information","twitter_post"], template=summary_template
+        input_variables=["information","twitter_post"], template=summary_template,
+        partial_variables={"format_instructions":summary_parser.get_format_instructions()},
     )
     
