df232fd723482252a94e2666d781ad545073e552
Khanhduong123 <khanhbaoduong789@gmail.com>
2025-05-15 08:53:53 +0700

    khanhdb: update custom prompt

--- document_assistant/main.py
+++ document_assistant/main.py
@@ -2,8 +2,14 @@ import time
 import os
 import streamlit as st
 from backend.core import run_llm
+from typing import List
+from typing import List, Dict, Any
 
 st.header("Document Search with Langchain and Pinecone")
+
+def format_chat_history(chat_history: List[Dict[str, Any]] = []) -> str:
+    return "\n".join(f"{role.capitalize()}: {msg}" for role, msg in chat_history)
+
 
 # Khởi tạo session state
 if "chat_answer_history" not in st.session_state:
@@ -20,15 +26,16 @@ with st.form("chat_form", clear_on_submit=True):
 
 if submitted and prompt:
     with st.spinner("Searching..."):
-        result = run_llm(prompt, chat_history=st.session_state.chat_history)
-        source = set([doc.page_content for doc in result['source_documents']])
+        formatted_chat_history = format_chat_history(st.session_state.chat_history)
+        result = run_llm(prompt, chat_history=formatted_chat_history)
+        # source = set(doc["page_content"] for doc in result["source_documents"])
 
-        formatted_result = f"{result['answer']} \n\n"
+        formatted_result = f"{result.answer} \n\n"
 
         st.session_state.user_prompt_history.append(prompt)
         st.session_state.chat_answer_history.append(formatted_result)
         st.session_state.chat_history.append(("human", prompt))
-        st.session_state.chat_history.append(("ai", result['answer']))
+        st.session_state.chat_history.append(("ai", result.answer))
 
 # Hiển thị lịch sử chat
 if st.session_state.chat_answer_history:
